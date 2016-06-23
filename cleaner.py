f085d1d350e32f00495f0bd0ec130f15198e39fe
arsh23 <programmer.arsh@gmail.com>
2016-06-23 17:01:51 +0530

    regex for size

--- cleaner.py
+++ cleaner.py
@@ -1,1 +1,46 @@
 import re
+import json
+
+data = {}
+phones = {}
+
+def load_data():
+    global data
+    with open('data2.json', 'r') as fp:
+        data = json.load(fp)
+
+    [ [ phones.update({x:data[y][x]}) for x in data[y].keys() ] for y in data.keys() ]
+
+
+def extract_size():
+    errors = [ x for x in phones.keys() if phones[x]['Dimensions'] in [[],['-']] ]
+    valids = [ (x,phones[x]['Dimensions'][0]) for x in phones.keys() if phones[x]['Dimensions'] not in [[],['-']] ]
+
+    regex = re.compile(r"^([\d\.]+) x ([\d\.]+) x ([\d\.]+) mm")
+    for name,value in valids:
+        result = regex.findall(value)
+        if result == []:
+            errors.append(name)
+            continue
+        phones[name]['Dimensions'] = {}
+        phones[name]['Dimensions'].update({ 'Height': float(result[0][0]), 'Length': float(result[0][1]), 'Width': float(result[0][2]) })
+
+    for name in errors:
+        phones[name]['Dimensions'] = {}
+        phones[name]['Dimensions'].update({ 'Height': -1, 'Length': -1, 'Width': -1 })
+
+    #thickest
+    # y = [ (x,phones[x]['Dimensions']['Width']) for x in phones.keys() ]
+    # z = sorted(y, key=lambda x: x[1],reverse=True)
+
+    #thinest
+    # y = [ (x,phones[x]['Dimensions']['Width']) for x in phones.keys() ]
+    # z = sorted(y, key=lambda x: x[1])
+
+    #smallest area
+    # y = [ (x,phones[x]['Dimensions']['Height']*phones[x]['Dimensions']['Length']) for x in phones.keys() ]
+    # z = sorted(y, key=lambda x: x[1])
+
+    #largest area
+    # y = [ (x,phones[x]['Dimensions']['Height']*phones[x]['Dimensions']['Length']) for x in phones.keys() ]
+    # z = sorted(y, key=lambda x: x[1],reverse=True)
