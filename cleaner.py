6cdcfb979dba3f2fabe949349e208566c2e628d1
arsh23 <programmer.arsh@gmail.com>
2016-06-24 19:21:13 +0530

    added date regex

--- cleaner.py
+++ cleaner.py
@@ -77,3 +77,30 @@ def extract_screen_size():
     for name in errors:
         phones[name]['Screen'] = {'Size':-1,'s2b_ratio':-1}
         phones[name].pop('Size',None)
+
+def extract_date():
+    # year - YYYY
+    # all test cases - http://regexr.com/3dmjp
+    # month - MM
+    # all test cases - http://regexr.com/3dmk8
+    errors = [ x for x in phones.keys() if phones[x]['Announced'] in [[],['-']] ]
+    valids = [ (x,phones[x]['Announced'][0]) for x in phones.keys() if phones[x]['Announced'] not in [[],['-']] ]
+
+    regex = re.compile(r"([\d]{4})")
+    for name,value in valids:
+        result = regex.findall(value)
+        phones[name]['Date'] = {}
+        if result == []: errors.append(name)
+        else: phones[name]['Date']['Year'] = int(result[0])
+        phones[name].pop('Announced',None)
+
+    regex = re.compile(r"([Jj]anuary|[Ff]ebruary|[Mm]arch|[Aa]pril|[Mm]ay|[Jj]une|[Jj]uly|[Aa]ugust|[Ss]eptember|[Oo]ctober|[Nn]ovember|[Dd]ecember)")
+    months = {'January':1,'February':2,'March':3,'April':4,'May':5,'June':6,'July':7,'August':8,'September':9,'October':10,'November':11,'December':12,}
+    for name,value in valids:
+        result = regex.findall(value)
+        if result == []: phones[name]['Date']['Month'] = -1
+        else: phones[name]['Date']['Month'] = months[result[0].capitalize()]
+
+    for name in errors:
+        phones[name]['Date'] = {'Year':-1,'Month':-1}
+        phones[name].pop('Announced',None)
